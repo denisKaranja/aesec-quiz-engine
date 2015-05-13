d41b1b0dfec467e6c1e68d85eb3ef1f581093bd1
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-13 15:52:33 +0300

    User details extraction
    Message, phone number, keyword and sender  extracted

--- app/application/controllers/home.php
+++ app/application/controllers/home.php
@@ -12,6 +12,38 @@ class Home extends CI_Controller
 
 	public function index()
 	{
-		echo "Home you are...";
+		# credentials
+		$username = "aiesec";
+		$apikey = "63bea0c464119b566dbc6f93246abffdae17216ebbac2e6ef770e58f446a9cb9";
+
+		#details from the user
+		$phone_number = $this->input->post('from');
+		$sender = $this->input->post('to');//shot code(sender)
+		$user_message = trim(strtolower($this->input->post('text')));
+
+    $message_from_user = substr($user_message, 0, 5);
+    $succeeding_msg = substr($user_message, 5);
+
+    $current_date_time = date("Y-m-d H:i:s");
+
+
+		
 	}
+
+	public function send_sms()
+	{
+
+	}
+
+	public function receive_sms()
+	{
+
+	}
+
+	public function get_question()
+	{
+
+	}
+
+
 }
